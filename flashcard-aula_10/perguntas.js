bc9e0bb72014a2d37901912f8491b9f74bb55c64
mqrbn7 <matheus.querubin@escola.pr.gov.br>
2025-07-05 19:58:20 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ciências',
+    'Qual é o estado da matéria do ar?',
+    'Gasoso.'
+)
+
+criaCartao(
+    'Esporte',
+    'Esporte e saúde mental?',
+    'Melhora o humor, reduz estresse e ansiedade.'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quem escreveu Dom Casmurro?',
+    'Machado De Assis.'
+)
+
+criaCartao(
+    'Esporte',
+    'Ética no esporte?',
+    'Garante justiça e crdebilidade'
+)
